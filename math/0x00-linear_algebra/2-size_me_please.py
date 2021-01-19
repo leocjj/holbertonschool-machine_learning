d04e77def91fe7821fda77007234be03f490bc92
Leonardo Calderon J <leocj@hotmail.com>
2021-01-18 21:02:03 -0500

    task 2

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" 0x00-linear_algebra Task 2 """
+def matrix_shape(matrix):
+    """ calculates the shape of a matrix """
+    shape = []
+    if isinstance(matrix[0], int):
+        return len(matrix)
+    if isinstance(matrix[0], list):
+        shape.append(len(matrix))
+        len1 = matrix_shape(matrix[0])
+        if isinstance(len1, int):
+            shape.append(len1)
+        if isinstance(len1, list):
+            shape.extend(len1)
+        return shape
