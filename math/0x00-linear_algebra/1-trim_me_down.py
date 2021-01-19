b055beac07583a00c1a992c0ae59a6763ad948b0
Leonardo Calderon J <leocj@hotmail.com>
2021-01-18 19:12:07 -0500

    Delete 1-trim_me_down.py

--- math/0x00-linear_algebra/1-trim_me_down.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
-
-the_middle = [[row[len(matrix) - 1], row[len(matrix)]] for row in matrix]
-print("The middle columns of the matrix are: {}".format(the_middle))
-
