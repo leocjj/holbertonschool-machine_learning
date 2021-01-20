6a13f2d70d79408369862426cef66e773fa493a9
leocjj <leocj@hotmail.com>
2021-01-20 13:02:49 -0500

    Task 7

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" 0x00-linear_algebra Task 7 """
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """ concatenates two matrices along a specific axis"""
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
+        # return [*deepcopy(mat1), *deepcopy(mat2)]
+        up_mat = [sublist[:] for sublist in mat1]
+        down_mat = [sublist[:] for sublist in mat2]
+        return [*up_mat, *down_mat]
+    elif axis == 1 and len(mat1) == len(mat2):
+        return [[*mat1[i], *mat2[i]] for i in range(len(mat1))]
+    else:
+        return None
