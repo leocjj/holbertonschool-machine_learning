b985d6e3985746b33c42d871f9d4a8d37584d6ab
leocjj <leocj@hotmail.com>
2021-01-20 16:14:00 -0500

    Task 16

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
 """ 0x00-linear_algebra Task 16 """
-from numpy import array
 
 
 def add_matrices(mat1, mat2):
     """  that adds two matrices. """
-    m1 = array(mat1)
-    m2 = array(mat2)
-    try:
-        result = m1 + m2
-    except ValueError:
-        return None
-    return result
+    return None
