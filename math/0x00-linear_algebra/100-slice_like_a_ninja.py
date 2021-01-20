267ac16b9beaf7e6bf1696930080a99322b8cb0c
leocjj <leocj@hotmail.com>
2021-01-20 15:44:58 -0500

    Task 16

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 0x00-linear_algebra Task 15 """
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    """  that slices a matrix along specific axes. """
+    return None
