994ced4f614436d8613fb269d7b5546e4685006c
Leonardo Calderon J <leocj@hotmail.com>
2021-01-20 12:50:37 -0500

    Task 2

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
-"""function get the shape of a matrix"""
+""" calculates the shape of a matrix """
 
 
 def matrix_shape(matrix):
     """ calculates the shape of a matrix """
     shape = []
-    if not matrix:
-        return 0
-    if isinstance(matrix[0], int):
-        return len(matrix)
-    if isinstance(matrix[0], list):
+
+    while type(matrix) == list:
         shape.append(len(matrix))
-        len1 = matrix_shape(matrix[0])
-        if isinstance(len1, int):
-            shape.append(len1)
-        if isinstance(len1, list):
-            shape.extend(len1)
-        return shape
+        if type(matrix[0]) == list:
+            matrix = matrix[0]
+        else:
+            break
+    return shape
