08e558b7c681f44c30732145b306949e708dd820
leocjj <leocj@hotmail.com>
2021-01-20 12:17:39 -0500

    Task 02

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
-    """ calculates the shape of a matrix. """
+    """ Calculates the shape of a matrix
+    Args:
+        matrix ([int]): contains all elements
+    """
+    rows = len(matrix)
+    items = 0
+    columns = 0
     shape = []
-    if not matrix:
-        return 0
-    if isinstance(matrix[0], int):
-        return len(matrix)
-    if isinstance(matrix[0], list):
-        shape.append(len(matrix))
-        len1 = matrix_shape(matrix[0])
-        if isinstance(len1, int):
-            shape.append(len1)
-        if isinstance(len1, list):
-            shape.extend(len1)
-        return shape
+
+    for item in matrix:
+        columns = len(item)
+        for sub_item in item:
+            if isinstance(sub_item, list):
+                items = len(sub_item)
+
+    shape.append(rows)
+    shape.append(columns)
+    if items != 0:
+        shape.append(items)
+    return shape
