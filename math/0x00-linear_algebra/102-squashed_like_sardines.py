a207cc7189815ae1d7ce5913e306379574cc0bec
leocjj <leocj@hotmail.com>
2021-01-20 16:12:00 -0500

    Task 17

---
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" 0x00-linear_algebra Task 17 """
+
+
+def cat_matrices(mat1, mat2, axis=0):
+    """  that concatenates two matrices along a specific axis. """
+    return None
