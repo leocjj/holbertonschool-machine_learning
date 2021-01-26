99aca0036e3b2802d657f41af0d777b953092fc0
leocjj <leocj@hotmail.com>
2021-01-25 22:20:38 -0500

    Create 0-line.py

---
+++ math/0x01-plotting/0-line.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 0x00-linear_algebra Task 14 """
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """  that performs matrix multiplication. """
+    return mat1 @ mat2
