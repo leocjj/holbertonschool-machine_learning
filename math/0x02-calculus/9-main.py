1476b23399ba037386fda7cb6d0034a5a7cc6ebe
leocjj <leocj@hotmail.com>
2021-01-30 19:26:59 -0500

    Task 9

--- math/0x02-calculus/9-main.py
+++ math/0x02-calculus/9-main.py
@@ -4,3 +4,15 @@ summation_i_squared = __import__('9-sum_total').summation_i_squared
 
 n = 5
 print(summation_i_squared(n))
+n = 0
+print(summation_i_squared(n))
+n = -1
+print(summation_i_squared(n))
+n = 1
+print(summation_i_squared(n))
+n = 100
+print(summation_i_squared(n))
+n = 1.5
+print(summation_i_squared(n))
+n = 2.0
+print(summation_i_squared(n))
