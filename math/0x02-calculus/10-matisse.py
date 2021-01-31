df6a2b0158baa357868e430cea366ddb97722955
leocjj <leocj@hotmail.com>
2021-01-30 20:03:40 -0500

    Task 10

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" 0x02. Calculus Task 10 """
+
+
+def poly_derivative(poly):
+    """  that calculates the derivative of a polynomial """
+    result = []
+    if not poly:
+        return None
+    if len(poly) <= 1:
+        return [0]
+    for power, coefficient in enumerate(poly):
+        if power >= 1:
+            result.append(coefficient * power)
+    return result
