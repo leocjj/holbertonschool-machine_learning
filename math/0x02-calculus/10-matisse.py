2d1309dd76b15483f8ce6080a264d71d504b4c16
leocjj <leocj@hotmail.com>
2021-01-30 22:04:15 -0500

    Task 10

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -5,8 +5,10 @@
 def poly_derivative(poly):
     """  that calculates the derivative of a polynomial """
     result = []
-    if not poly:
+    if not poly or not isinstance(poly, list):
         return None
+    if len(poly) <= 1:
+        return [0]
     for power, coefficient in enumerate(poly):
         if power >= 1:
             result.append(coefficient * power)
