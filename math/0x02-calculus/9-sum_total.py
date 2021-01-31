5d0618440e57c6f2e93f3aac1347752c1b7c2851
leocjj <leocj@hotmail.com>
2021-01-30 19:22:22 -0500

    Task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -3,6 +3,7 @@
 
 
 def summation_i_squared(n):
+    """ that calculates sum of i power of 2 from 1 to n """
     if not isinstance(n, int):
         return None
     return int(sum([i**2 for i in range(1, n + 1)]))
