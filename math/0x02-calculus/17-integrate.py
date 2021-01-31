18b9fffd59b0b141c496598f9d03e925a62d45bf
leocjj <leocj@hotmail.com>
2021-01-30 22:48:42 -0500

    Task 17

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,12 +6,12 @@ def poly_integral(poly, C=0):
     """  that calculates the integral of a polynomial """
     if not poly or not isinstance(poly, list) or not isinstance(C, int):
         return None
-    if len(poly) <= 1:
-        return [C]
     result = [C]
     for power, coefficient in enumerate(poly):
         temp = coefficient / (power + 1)
         if temp.is_integer():
             temp = int(temp)
         result.append(temp)
+    if result[-1] == 0:
+        result.pop()
     return result
--- math/0x02-calculus/17-main.py
+++ math/0x02-calculus/17-main.py
@@ -4,3 +4,26 @@ poly_integral = __import__('17-integrate').poly_integral
 
 poly = [5, 3, 0, 1]
 print(poly_integral(poly))
+poly = []
+print(poly_integral(poly))
+poly = [0]
+print(poly_integral(poly))
+poly = [5]
+print(poly_integral(poly))
+poly = [0, 1]
+print(poly_integral(poly))
+poly = [3, 0, 1]
+print(poly_integral(poly))
+print()
+poly = [5, 3, 0, 1]
+print(poly_integral(poly, C=2))
+poly = []
+print(poly_integral(poly, C=2))
+poly = [0]
+print(poly_integral(poly, C=2))
+poly = [5]
+print(poly_integral(poly, C=2))
+poly = [0, 1]
+print(poly_integral(poly, C=2))
+poly = [3, 0, 1]
+print(poly_integral(poly, C=2))
