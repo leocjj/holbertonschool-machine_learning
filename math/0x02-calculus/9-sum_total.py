ae851e642c615cb85c3354346ea9bfd8445062c1
leocjj <leocj@hotmail.com>
2021-01-30 19:18:12 -0500

    Task 9

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 0x02. Calculus Task 9 """
+
+
+def summation_i_squared(n):
+    if not isinstance(n, int):
+        return None
+    return int(sum([i**2 for i in range(1, n + 1)]))
