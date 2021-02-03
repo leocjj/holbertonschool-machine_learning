6c5792cd203ccbd71177bd2079988e54f3a1cf40
leocjj <leocj@hotmail.com>
2021-02-02 21:51:46 -0500

    Task 1

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -8,6 +8,7 @@ class Poisson:
     e = 2.7182818285
 
     def __init__(self, data=None, lambtha=1.):
+        """  init method """
         if data is None:
             if lambtha <= 0:
                 raise ValueError('lambtha must be a positive value')
@@ -20,16 +21,22 @@ class Poisson:
                 raise ValueError('data must contain multiple values')
             self.__lambtha = sum(data) / len(data)
 
-    @property  # Property wrap
-    def lambtha(self):  # getter method
+    @property
+    def lambtha(self):
+        """  getter method """
         return self.__lambtha
 
     def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of “successes”
+        :param k:  is the number of “successes”
+        :return: PMF value for k
+        """
         k = int(k)
-        factorial_of_k = 1
+        fact_k = 1
         if k < 0:
             return 0
         elif k > 0:
             for i in range(1, k + 1):
-                factorial_of_k *= i
-        return pow(self.e, - self.__lambtha) * pow(self.__lambtha, k) / factorial_of_k
+                fact_k *= i
+        return pow(self.e, - self.__lambtha) * pow(self.__lambtha, k) / fact_k
