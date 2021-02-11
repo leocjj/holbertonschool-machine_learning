af4d06aedf56ec09a50e7da1b86ac6dba6d4c750
leocjj <leocj@hotmail.com>
2021-02-11 00:18:10 -0500

    Task 2

---
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+""" 0x01. Classification """
+import numpy as np
+
+
+def sigmoid(x):
+    """
+    https://stackoverflow.com/questions/3985619/
+    how-to-calculate-a-logistic-sigmoid-function-in-python
+    Y = 1 / (1 + np.exp(-x))
+    :param x: int or array. Use math.ext instead of np.exp for integers.
+    :return: sigmoid function of x
+    """
+    return np.exp(-np.logaddexp(0, -x))
+
+
+class Neuron:
+    """ Class that defines a single neuron performing binary classification """
+
+    def __init__(self, nx):
+        """
+        Defines a single neuron performing binary classification
+        :param nx: number of input features to the neuron
+        """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.normal(size=nx).reshape(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """  getter method """
+        return self.__W
+
+    @property
+    def b(self):
+        """  getter method """
+        return self.__b
+
+    @property
+    def A(self):
+        """  getter method """
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron,
+            using a sigmoid activation function.
+        :param X: is a numpy.ndarray with shape (nx, m) that contains the input
+            nx is the number of input features to the neuron,
+            m is the number of examples.
+        :return:  the private attribute __A
+        """
+        self.__A = sigmoid(np.matmul(self.W, X))
+        return self.__A
