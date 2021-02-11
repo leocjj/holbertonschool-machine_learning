40b0ffa5437e747a8ff0710018196ca366ce5f76
leocjj <leocj@hotmail.com>
2021-02-11 01:43:42 -0500

    Update 0-neuron.py

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -15,6 +15,6 @@ class Neuron:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        self.W = np.random.normal(size=nx).reshape(1, nx)
+        self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
