53e3e4834b5331d771bfd2c15c23b49e2298ad71
leocjj <leocj@hotmail.com>
2021-02-13 23:44:17 -0500

    Task 5

---
+++ supervised_learning/0x01-classification/5-neuron.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+""" 0x01. Classification """
+import numpy as np
+
+
+def sigmoid(x):
+    """
+    https://stackoverflow.com/questions/3985619/
+    how-to-calculate-a-logistic-sigmoid-function-in-python
+    Y = 1. / (1. + np.exp(-x))
+    :param x: int or array. Use math.ext instead of np.exp for integers.
+    :return: sigmoid function of x
+    """
+    return np.exp(-np.logaddexp(0., -x))
+
+
+class Neuron:
+    """ Class that defines a single neuron performing binary classification """
+
+    def __init__(self, nx):
+        """
+        Defines a single neuron performing binary classification
+        :param nx: number of input features to the neuron
+        """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """  getter method """
+        return self.__W
+
+    @property
+    def b(self):
+        """  getter method """
+        return self.__b
+
+    @property
+    def A(self):
+        """  getter method """
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron,
+            using a sigmoid activation function.
+        :param X: is a numpy.ndarray with shape (nx, m) that contains the input
+            nx is the number of input features to the neuron,
+            m is the number of examples.
+        :return: the private attribute __A
+        """
+        self.__A = sigmoid(np.matmul(self.W, X) + self.b)
+        return self.A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression
+        https://datascience.stackexchange.com/questions/22470/
+        python-implementation-of-cost-function-in-logistic-regression-why-dot-multiplic
+        :param Y: is a numpy.ndarray with shape (1, m) that contains the
+            correct labels for the input data
+        :param A: is a numpy.ndarray with shape (1, m) containing the activated
+            output of the neuron for each example
+        :return: return -1 / Y.shape[1] * np.sum( np.multiply(np.log(A), Y) +
+            np.multiply(np.log(1.0000001 - A), (1.0000001 - Y)))
+        """
+        return -1 / Y.shape[1] * np.sum(
+            np.multiply(np.log(A), Y) +
+            np.multiply(np.log(1.0000001 - A), (1 - Y))
+        )
+
+    def evaluate(self, X, Y):
+        """
+        Evaluates the neuron’s predictions
+        :param X: is a numpy.ndarray with shape (nx, m) that contains the input
+            nx is the number of input features to the neuron,
+            m is the number of examples.
+        :param Y: is a numpy.ndarray with shape (1, m) that contains the
+            correct labels for the input data
+        :return: tuple with the neuron’s prediction and the cost of the network
+            prediction is numpy.ndarray with shape (1, m) containing the
+            predicted labels for each example and the label values should be 1
+            if the output of the network is >= 0.5 and 0 otherwise
+        """
+        self.forward_prop(X)
+        return np.heaviside(self.A - 0.5, 1).astype(int), self.cost(Y, self.A)
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """
+        Calculates one pass of gradient descent on the neuron
+        :param X: is a numpy.ndarray with shape (nx, m) that contains the input
+            nx is the number of input features to the neuron,
+            m is the number of examples.
+        :param Y: is a numpy.ndarray with shape (1, m) that contains the
+            correct labels for the input data
+        :param A: is a numpy.ndarray with shape (1, m) containing the activated
+            output of the neuron for each example
+        :param alpha: is the learning rate
+        :return: Nothing. Updates the private attributes __W and __b
+        """
+        m = Y.shape[1]
+        dZ = A - Y
+        self.__W -= (alpha * (1 / m) * np.matmul(X, dZ.T)).T
+        self.__b -= (alpha * (1 / m) * np.sum(dZ))
