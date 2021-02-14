e1a25ec0eb8a2945fbdba53bdc2ab6a3719b3138
leocjj <leocj@hotmail.com>
2021-02-14 02:33:26 -0500

    Update 13-neural_network.py

--- supervised_learning/0x01-classification/13-neural_network.py
+++ supervised_learning/0x01-classification/13-neural_network.py
@@ -137,8 +137,13 @@ class NeuralNetwork:
 
         m = Y.shape[1]
         dZ2 = A2 - Y
-        self.__W2 -= (alpha * ((1 / m) * np.matmul(A1, dZ2.T))).T
-        self.__b2 -= (alpha * ((1 / m) * np.sum(dZ2, axis=1, keepdims=True)))
+        dW2 = (1 / m) * np.matmul(A1, dZ2.T)
+        db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
         dZ1 = np.matmul(self.__W2.T, dZ2) * (A1 * (1 - A1))
-        self.__W1 -= (alpha * ((1 / m) * np.matmul(dZ1, X.T)))
-        self.__b1 -= (alpha * ((1 / m) * np.sum(dZ1, axis=1, keepdims=True)))
+        dW1 = (1 / m) * np.matmul(dZ1, X.T)
+        db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+
+        self.__W1 = self.__W1 - (alpha * dW1)
+        self.__b1 = self.__b1 - (alpha * db1)
+        self.__W2 = self.__W2 - (alpha * dW2).T
+        self.__b2 = self.__b2 - (alpha * db2)
