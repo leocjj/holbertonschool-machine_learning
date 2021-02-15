51a20f8171bb111bdd823aae56e8292ce596da25
leocjj <leocj@hotmail.com>
2021-02-15 14:53:11 -0500

    Update 16-deep_neural_network.py

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -34,10 +34,11 @@ class DeepNeuralNetwork:
                 np.sqrt(2 / layers[i - 1])
             self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
         """
-        for i in range(1, self.L):
+        for i in range(self.L):
             w_i = "W" + str(i + 1)
             b_i = "b" + str(i + 1)
 
-            self.weights[w_i] = np.random.randn(layers[i], layers[i - 1])\
-                * np.sqrt(2 / layers[i - 1])
+            if i > 0:
+                self.weights[w_i] = np.random.randn(layers[i], layers[i - 1])\
+                    * np.sqrt(2 / layers[i - 1])
             self.weights[b_i] = np.zeros((layers[i], 1))
