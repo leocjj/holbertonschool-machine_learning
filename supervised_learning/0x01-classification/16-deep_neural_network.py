036983cdb77b701802a154ac69e23e593ef57c5b
leocjj <leocj@hotmail.com>
2021-02-15 12:40:11 -0500

    Update 16-deep_neural_network.py

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -19,9 +19,10 @@ class DeepNeuralNetwork:
             raise ValueError('nx must be a positive integer')
         if not isinstance(layers, list) or not list:
             raise TypeError('layers must be a list of positive integers')
-        if len(layers) == 0:
-            raise TypeError("layers must be a list of positive integers")
-        # Hidden layer
+        if not np.issubdtype(np.array(layers).dtype, np.integer) or\
+                not all(np.array(layers) >= 1):
+            raise TypeError('layers must be a list of positive integers')
+
         self.L = len(layers)
         self.cache = {}
         self.weights = {'W1': np.random.randn(layers[0], nx) *
