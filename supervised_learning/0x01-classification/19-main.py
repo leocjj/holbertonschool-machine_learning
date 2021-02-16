af91ed8b7e9c75175f810d5b8208780ccfefeb57
leocjj <leocj@hotmail.com>
2021-02-15 21:19:27 -0500

    Task 19

--- supervised_learning/0x01-classification/19-main.py
+++ supervised_learning/0x01-classification/19-main.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-Deep = __import__('18-deep_neural_network').DeepNeuralNetwork
+Deep = __import__('19-deep_neural_network').DeepNeuralNetwork
 
 lib_train = np.load('../data/Binary_Train.npz')
 X_3D, Y = lib_train['X'], lib_train['Y']
@@ -10,11 +10,6 @@ X = X_3D.reshape((X_3D.shape[0], -1)).T
 
 np.random.seed(0)
 deep = Deep(X.shape[0], [5, 3, 1])
-deep._DeepNeuralNetwork__weights['b1'] = np.ones((5, 1))
-deep._DeepNeuralNetwork__weights['b2'] = np.ones((3, 1))
-deep._DeepNeuralNetwork__weights['b3'] = np.ones((1, 1))
-A, cache = deep.forward_prop(X)
-print(A)
-print(cache)
-print(cache is deep.cache)
-print(A is cache['A3'])
+A, _ = deep.forward_prop(X)
+cost = deep.cost(Y, A)
+print(cost)
