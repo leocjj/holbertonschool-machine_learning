1aa540f7f1648ea8477fdf994a8415eed2f74286
leocjj <leocj@hotmail.com>
2021-02-18 22:08:41 -0500

    Create 7-evaluate.py

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" 0x02. Tensorflow """
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """
+    Function that evaluates the output of a neural network.
+    :param X: is a numpy.ndarray containing the input data to evaluate
+    :param Y: is a numpy.ndarray containing the one-hot labels for X
+    :param save_path: is the location to load the model from
+    :return: the network’s prediction, accuracy, and loss, respectively.
+    """
+
+    pass
+    return X, Y, save_path, tf
