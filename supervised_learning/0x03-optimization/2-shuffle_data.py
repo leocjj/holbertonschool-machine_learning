815d5b90d30d2020cd256f3b2dddfb68ccaebd68
leocjj <leocj@hotmail.com>
2021-02-25 22:10:54 -0500

    Task 02

---
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" 0x03. Optimization """
+import numpy as np
+
+
+def shuffle_data(X, Y):
+    """
+    Normalizes (standardizes) a matrix:
+    :param X: is the numpy.ndarray of shape (m, nx) to normalize
+        m is the number of data points
+        nx is the number of features
+    :param Y: is the second numpy.ndarray of shape (m, ny) to shuffle
+        m is the same number of data points as in X
+        ny is the number of features in Y
+    :return: shuffled X and Y matrices
+    """
+
+    shuffler = np.random.permutation(len(X))
+
+    return X[shuffler], Y[shuffler]
