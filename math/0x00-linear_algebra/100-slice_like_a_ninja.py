7dde5fa22b5c463c09ac980cb977284636d32c6e
leocjj <leocj@hotmail.com>
2021-04-14 14:03:46 -0500

    Update 100-slice_like_a_ninja.py

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -4,4 +4,9 @@
 
 def np_slice(matrix, axes={}):
     """  that slices a matrix along specific axes. """
-    return None
+
+    slicer = [slice(None)] * len(matrix.shape)
+    for key, value in axes.items():
+        slicer[key] = slice(*value)
+
+    return matrix[tuple(slicer)]
