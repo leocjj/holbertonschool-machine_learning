556b69f032f8b823ac3d35092de19f1d3a24e094
leocjj <leocj@hotmail.com>
2021-04-20 00:49:44 -0500

    Update 101-pca.py

--- math/0x01-plotting/101-pca.py
+++ math/0x01-plotting/101-pca.py
@@ -12,7 +12,14 @@ norm_data = data - data_means
 _, _, Vh = np.linalg.svd(norm_data)
 pca_data = np.matmul(norm_data, Vh[:3].T)
 
-plt.title('PCA of Iris Dataset')
+fig = plt.figure()
+ax = Axes3D(fig)#fig.add_subplot(111, projection='3d')
+ax.scatter(pca_data[:,0], pca_data[:,1], zs=pca_data[:,2], zdir='z', s=20, c=labels, depthshade=True, cmap='plasma')
+ax.set_title(label='PCA of Iris Dataset')
+ax.set_xlabel("U1")
+ax.set_ylabel("U2")
+ax.set_zlabel("U3")
+
 
 plt.show()
 plt.close()
