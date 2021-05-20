8624a7ed3fcc5e01246ea498f454c4f49aebb6cb
leocjj <leocj@hotmail.com>
2021-05-20 18:40:50 -0500

    Update 13-batch_norm.py

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -20,7 +20,7 @@ def batch_norm(Z, gamma, beta, epsilon):
 
     mean = np.sum(Z, axis=0) / Z.shape[0]
     variance = np.sum(np.power(Z - mean, 2), axis=0) / Z.shape[0]
-    Z_normalized = (Z - mean) / np.sqrt(variance + epsilon)
+    Z_normalized = (Z - mean) / (np.sqrt(variance) + epsilon)
     Z_scaled = gamma * Z_normalized + beta
 
     return Z_scaled
