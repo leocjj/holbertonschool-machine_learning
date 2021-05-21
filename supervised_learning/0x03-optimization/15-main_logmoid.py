44ac0a6a85c336af4dac98231708743ff9ac791d
leocjj <leocj@hotmail.com>
2021-05-20 23:53:25 -0500

    Create 15-main_logmoid.py

---
+++ supervised_learning/0x03-optimization/15-main_logmoid.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import tensorflow as tf
+# from logmoid import logmoid
+
+model = __import__('15-model').model
+
+def one_hot(Y, classes):
+    """convert an array to a one-hot matrix"""
+    oh = np.zeros((Y.shape[0], classes))
+    oh[np.arange(Y.shape[0]), Y] = 1
+    return oh
+
+def logmoid(z):
+    """
+    Piecewise-defined function using ln(x+1) for x equals or greater than zero
+    and -ln(-x+1) for x less than zero.
+    :param z: int or array. Use math.log instead of np.log for integers.
+    :return: logmoid function of x
+    """
+
+    # TODO: CREATE THIS FUNCTIONS AS A MODULE WITH cython
+    return tf.cond(z < 0., lambda: -tf.math.log(1. - z), lambda: tf.math.log(1. + z))
+
+
+if __name__ == '__main__':
+    lib= np.load('../data/MNIST.npz')
+    X_train_3D = lib['X_train']
+    Y_train = lib['Y_train']
+    X_train = X_train_3D.reshape((X_train_3D.shape[0], -1))
+    Y_train_oh = one_hot(Y_train, 10)
+    X_valid_3D = lib['X_valid']
+    Y_valid = lib['Y_valid']
+    X_valid = X_valid_3D.reshape((X_valid_3D.shape[0], -1))
+    Y_valid_oh = one_hot(Y_valid, 10)
+
+    layer_sizes = [256, 256, 10]
+    activations = [tf.math.sin, tf.math.sin, None]
+
+    np.random.seed(0)
+    tf.set_random_seed(0)
+    save_path = model((X_train, Y_train_oh), (X_valid, Y_valid_oh), layer_sizes,
+                                 activations, save_path='./model.ckpt')
+    print('Model saved in path: {}'.format(save_path))
