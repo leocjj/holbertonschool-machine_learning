0e3f587eca8abb57a8abaf9489475a135c3ad06d
leocjj <leocj@hotmail.com>
2021-05-24 22:59:10 -0500

    Task 4

---
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" 0x04. Error Analysis """
+
+import numpy as np
+
+
+def f1_score(confusion):
+    """
+    Function that calculates the F1 score of a confusion matrix
+    :param confusion: a confusion numpy.ndarray of shape (classes, classes)
+        where row indices represent the correct labels and column indices
+        represent the predicted labels
+        classes is the number of classes
+    :return: numpy.ndarray of shape (classes,) containing the F1 score of
+        each class
+    """
+
+    TP = np.diag(confusion)
+    PP = np.sum(confusion, axis=0)
+    P = np.sum(confusion, axis=1)
+
+    # TP / (TP + 0.5(FP + FN))
+    return 2 * TP / (PP + P)
