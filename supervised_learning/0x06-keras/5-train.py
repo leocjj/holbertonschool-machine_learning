91c0144c7c47351917932a155ed2fc8d67635a6c
leocjj <leocj@hotmail.com>
2021-06-01 14:53:25 -0500

    Create 5-train.py

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" 0x06. Keras """
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent
+    :param network: the model to train
+    :param data: numpy.ndarray of shape (m, nx) containing the input data
+    :param labels: a one-hot numpy.ndarray of shape (m, classes) containing the
+        labels of data
+    :param batch_size: size of the batch used for mini-batch gradient descent
+    :param epochs:number of passes through data for mini-batch gradient descent
+    :param validation_data: the data to validate the model with, if not None.
+    :param verbose: boolean, determines if output should be printed in training
+    :param shuffle: boolean, determines whether to shuffle the batches every
+        epoch. For reproducibility, set the default to False.
+    :return: the History object generated after training the model
+    """
+
+    return network.fit(x=data, y=labels, batch_size=batch_size,
+                       epochs=epochs, verbose=verbose,
+                       validation_data=validation_data, shuffle=shuffle)
