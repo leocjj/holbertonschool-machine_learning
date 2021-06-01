fedf9d5715ab076f003c69706679363fe7c05c67
leocjj <leocj@hotmail.com>
2021-06-01 18:05:10 -0500

    Update 12-test.py

--- supervised_learning/0x06-keras/12-test.py
+++ supervised_learning/0x06-keras/12-test.py
@@ -16,16 +16,3 @@ def test_model(network, data, labels, verbose=True):
     """
 
     return network.evaluate(x=data, y=labels, verbose=verbose)
-
-
-def load_config(filename):
-    """
-    Loads a model with a specific configuration:
-    :param filename: path of file containing the model’s configuration in JSON
-    :return: None
-    """
-
-    with open(filename, "r") as f:
-        network_string = f.read()
-
-    return K.models.model_from_json(network_string)
