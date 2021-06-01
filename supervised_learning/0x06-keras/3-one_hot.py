d834884d1d2301a83c9fa0712fc4f8b467136135
leocjj <leocj@hotmail.com>
2021-06-01 11:33:27 -0500

    Update 3-one_hot.py

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -8,7 +8,8 @@ def one_hot(labels, classes=None):
     """
     Converts a label vector into a one-hot matrix.
     The last dimension of the one-hot matrix must be the number of classes.
-    :param labels:
+    :param labels: a one-hot numpy.ndarray of shape (m, classes) containing the
+        labels of data
     :param classes:
     :return: one-hot matrix
     """
