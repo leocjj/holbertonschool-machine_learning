9965f800583338a0617b3b5bdc6dc7f5c3f242bb
leocjj <leocj@hotmail.com>
2021-06-17 23:56:16 -0500

    Update 7-densenet121.py

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-""" 0x08. Deep CNNs """
+"""DenseNet-121"""
+
+
 import tensorflow.keras as K
 dense_block = __import__('5-dense_block').dense_block
 transition_layer = __import__('6-transition_layer').transition_layer
@@ -7,22 +9,10 @@ transition_layer = __import__('6-transition_layer').transition_layer
 
 def densenet121(growth_rate=32, compression=1.0):
     """
-    Builds the DenseNet-121 architecture as described in Densely Connected
-        Convolutional Networks:
     growth_rate is the growth rate
     compression is the compression factor
-    You can assume the input data will have shape (224, 224, 3)
-    All convolutions are preceded by Batch Normalization and a rectified linear
-        activation (ReLU), respectively
-    All weights use he normal initialization
-    Use:
-        dense_block = __import__('5-dense_block').dense_block
-        transition_layer = __import__('6-transition_layer').transition_layer
-    Returns: the keras model
     """
-
     inputs = K.Input(shape=(224, 224, 3))
-
     out_l = K.layers.BatchNormalization(axis=3)(inputs)
     out_l = K.layers.Activation('relu')(out_l)
     out_l = K.layers.Conv2D(64, kernel_size=(7, 7), padding='same',
@@ -39,5 +29,4 @@ def densenet121(growth_rate=32, compression=1.0):
     out_l = K.layers.AvgPool2D((7, 7), padding='same')(out_l)
     out_l = K.layers.Dense(1000, activation='softmax')(out_l)
     model = K.Model(inputs, out_l)
-
     return model
