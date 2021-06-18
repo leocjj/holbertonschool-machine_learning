098f6973226903b3cf51a14a0d78f832123bc1af
leocjj <leocj@hotmail.com>
2021-06-18 00:09:59 -0500

    Update 7-densenet121.py

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -14,6 +14,9 @@ def densenet121(growth_rate=32, compression=1.0):
     growth_rate is the growth rate
     compression is the compression factor
     You can assume the input data will have shape (224, 224, 3)
+    All convolutions should be preceded by Batch Normalization and a rectified
+        linear activation (ReLU), respectively
+    All weights use he normal initialization
     """
     inputs = K.Input(shape=(224, 224, 3))
     out_l = K.layers.BatchNormalization(axis=3)(inputs)
