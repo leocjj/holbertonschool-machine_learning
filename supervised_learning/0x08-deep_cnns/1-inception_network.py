87fd5e82a6120ebfaa7edb416cda8676541ca017
leocjj <leocj@hotmail.com>
2021-06-17 22:15:01 -0500

    Update 1-inception_network.py

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -24,12 +24,12 @@ def inception_network():
     out = K.layers.Conv2D(192, 3, 1, activation='relu', padding='same')(out)
     out = K.layers.MaxPool2D(3, 2, padding='same')(out)
 
-    # Inception 3a, 3b
+    ''' Inception 3a, 3b '''
     out = inception_block(out, [64, 96, 128, 16, 32, 32])
     out = inception_block(out, [128, 128, 192, 32, 96, 64])
     out = K.layers.MaxPool2D(3, 2, padding='same')(out)
 
-    # Inception 4a, 4b, 4c, 4d, 4e
+    ''' Inception 4a, 4b, 4c, 4d, 4e '''
     out = inception_block(out, [192, 96, 208, 16, 48, 64])
     out = inception_block(out, [160, 112, 224, 24, 64, 64])
     out = inception_block(out, [128, 128, 256, 24, 64, 64])
@@ -37,13 +37,13 @@ def inception_network():
     out = inception_block(out, [256, 160, 320, 32, 128, 128])
     out = K.layers.MaxPool2D(3, 2, padding='same')(out)
 
-    # Inception 5a, 5b
+    ''' Inception 5a, 5b '''
     out = inception_block(out, [256, 160, 320, 32, 128, 128])
     out = inception_block(out, [384, 192, 384, 48, 128, 128])
     out = K.layers.AvgPool2D(7, 1)(out)
 
     out = K.layers.Dropout(.4)(out)
-    # Linear softmax
+    ''' Linear softmax '''
     out = K.layers.Dense(1000)(out)
 
     return K.Model(inputs, out)
