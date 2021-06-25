47dadc8b7e04925c1fe894d8b1003f552d27f7fc
leocjj <leocj@hotmail.com>
2021-06-24 21:30:55 -0500

    Update 0-transfer.py

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -20,43 +20,44 @@ def preprocess_data(X, Y):
     return X, Y
 
 
-(x_train, y_train), (x_test, y_test) = K.datasets.cifar10.load_data()
-x_train, y_train = preprocess_data(x_train, y_train)
-x_test, y_test = preprocess_data(x_test, y_test)
+if __name__ == '__main__':
+    (x_train, y_train), (x_test, y_test) = K.datasets.cifar10.load_data()
+    x_train, y_train = preprocess_data(x_train, y_train)
+    x_test, y_test = preprocess_data(x_test, y_test)
 
-base_model = K.applications.vgg16.VGG16(include_top=False, weights='imagenet',
-                                        pooling='avg',
-                                        classes=y_train.shape[1])
+    base_model = K.applications.vgg16.VGG16(include_top=False, weights='imagenet',
+                                            pooling='avg',
+                                            classes=y_train.shape[1])
 
-model = K.Sequential()
-model.add(K.layers.UpSampling2D())
-model.add(base_model)
-model.add(K.layers.Flatten())
-model.add(K.layers.Dense(256, activation=('relu')))
-model.add(K.layers.Dropout(0.5))
-model.add(K.layers.Dense(256, activation=('relu')))
-model.add(K.layers.Dropout(0.5))
-model.add(K.layers.Dense(10, activation=('softmax')))
-callback = []
-
-
-def rate_decay(epoch):
-    return 0.001 / (1 + 1 * 30)
+    model = K.Sequential()
+    model.add(K.layers.UpSampling2D())
+    model.add(base_model)
+    model.add(K.layers.Flatten())
+    model.add(K.layers.Dense(256, activation=('relu')))
+    model.add(K.layers.Dropout(0.5))
+    model.add(K.layers.Dense(256, activation=('relu')))
+    model.add(K.layers.Dropout(0.5))
+    model.add(K.layers.Dense(10, activation=('softmax')))
+    callback = []
 
 
-learning = K.callbacks.LearningRateScheduler(schedule=rate_decay, verbose=1)
-callback.append(learning)
-callback.append(K.callbacks.ModelCheckpoint('cifar10.h5',
-                                            monitor='val_accuracy',
-                                            save_best_only=True,
-                                            mode='max'))
+    def rate_decay(epoch):
+        """ Decay for learning rate """
+        return 0.001 / (1 + 1 * 30)
 
-model.compile(optimizer=K.optimizers.Adam(lr=2e-5),
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
 
-history = model.fit(x_train, y_train, epochs=5, batch_size=32,
-                    validation_data=(x_test, y_test))
-print("Saving model: cifar10.h5")
-model.save('cifar10.h5')
-print("Model saved.")
+    learning = K.callbacks.LearningRateScheduler(schedule=rate_decay, verbose=1)
+    callback.append(learning)
+    callback.append(K.callbacks.ModelCheckpoint('cifar10.h5',
+                                                monitor='val_accuracy',
+                                                save_best_only=True,
+                                                mode='max'))
+
+    model.compile(optimizer=K.optimizers.Adam(lr=2e-5),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    history = model.fit(x_train, y_train, epochs=1, batch_size=32,
+                        validation_data=(x_test, y_test))
+
+    model.save('cifar10.h5')
