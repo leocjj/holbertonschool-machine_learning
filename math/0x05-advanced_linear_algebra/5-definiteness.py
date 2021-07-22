1c8e9a44a2b777b4395ca75f4cead3818607b5b2
leocjj <leocj@hotmail.com>
2021-07-21 23:20:31 -0500

    Update 5-definiteness.py

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -22,8 +22,7 @@ def definiteness(matrix):
 
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
-    my_len = matrix.shape[0]
-    if len(matrix.shape) != 2 or my_len != matrix.shape[1]:
+    if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
         return None
     transpose = np.transpose(matrix)
     if not np.array_equal(transpose, matrix):
