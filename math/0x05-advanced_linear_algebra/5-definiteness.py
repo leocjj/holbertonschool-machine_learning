960f93eb00d8e8f497f5fb166917d756092911c9
leocjj <leocj@hotmail.com>
2021-07-21 23:15:50 -0500

    Update 5-definiteness.py

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -1,47 +1,39 @@
 #!/usr/bin/env python3
-""" 0x05-advanced_linear_algebra """
+"""
+Calculates the definiteness of a matrix
+"""
 
 import numpy as np
 
 
 def definiteness(matrix):
     """
-    calculates the definiteness of a matrix:
-
-    matrix is a numpy.ndarray of shape (n, n)
-    If matrix is not a numpy.ndarray, raise a TypeError with the message matrix
-        must be a numpy.ndarray
-    If matrix is not a valid matrix, return None
-    Return:
-        the string Positive definite, Positive semi-definite,
-        Negative semi-definite, Negative definite, or Indefinite if the matrix
-        is positive definite, positive semi-definite, negative semi-definite,
-        negative definite of indefinite, respectively
-    If matrix does not fit any of the above categories, return None
-    You may import numpy as np
+    Calculates the definiteness of a matrix
     """
-
+    # Type test
     if type(matrix) is not np.ndarray:
-        raise TypeError('matrix must be a numpy.ndarray')
+        raise TypeError("matrix must be a numpy.ndarray")
 
-    if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
+    # Square test
+    my_len = matrix.shape[0]
+    if len(matrix.shape) != 2 or my_len != matrix.shape[1]:
         return None
 
-    if matrix.shape[0] != matrix.shape[1]:
+    # Symmetry test
+    transpose = np.transpose(matrix)
+    if not np.array_equal(transpose, matrix):
         return None
 
-    if not np.array_equal(matrix.T, matrix):
-        return None
+    # Eigenvalues
+    w, _ = np.linalg.eig(matrix)
 
-    eg, _ = np.linalg.eig(matrix)
-
-    if all(eg > 0):
-        return "Positive definite"
-    if all(eg >= 0):
-        return "Positive semi-definite"
-    if all(eg < 0):
-        return "Negative definite"
-    if all(eg <= 0):
-        return "Negative semi-definite"
+    if all(w > 0):
+        return 'Positive definite'
+    elif all(w >= 0):
+        return 'Positive semi-definite'
+    elif all(w < 0):
+        return 'Negative definite'
+    elif all(w <= 0):
+        return 'Negative semi-definite'
     else:
-        return "Indefinite"
+        return 'Indefinite'
