54bad9b4825a7da37e408de0fd8066fc500ad53d
leocjj <leocj@hotmail.com>
2021-07-27 23:38:17 -0500

    Update multinormal.py

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -1,34 +1,35 @@
+
 #!/usr/bin/env python3
 """
-Represents a Multivariate Normal distribution
+that represents a Multivariate Normal distribution
 """
-
-
 import numpy as np
 
 
 class MultiNormal:
     """
-    Represents a Multivariate Normal distribution
+    Multinormal Class
     """
+
     def __init__(self, data):
         """
-        Class constructor
+        data is a numpy.ndarray of shape (d, n) containing the data set:
+        n is the number of data points
+        d is the number of dimensions in each data point
+        If data is not a 2D numpy.ndarray, raise a TypeError with the message
+        data must be a 2D numpy.ndarray
+        If n is less than 2, raise a ValueError with the message data must
+        contain multiple data points
         """
         if type(data) is not np.ndarray or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
 
-        d, n = data.shape
-
-        if n < 2:
+        if data.shape[1] < 2:
             raise ValueError("data must contain multiple data points")
 
-        self.mean = np.mean(data, axis=1).reshape(d, 1)
-
-        deviation = data - self.mean
-
-        self.cov = np.matmul(deviation, deviation.T) / (n - 1)
-
+        mean = np.mean(data, axis=1).reshape((data.shape[0], 1))
+        self.mean = mean
+        self.cov = np.matmul(data - self.mean, data.T) / (data.shape[1] - 1)
 
     def pdf(self, x):
         """
