50bf19423d72187ceb5d2293d3067c3c03d835e7
leocjj <leocj@hotmail.com>
2021-07-27 23:31:25 -0500

    Update multinormal.py

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -1,55 +1,63 @@
 #!/usr/bin/env python3
-"""Init of MultiNormal class"""
+"""
+Represents a Multivariate Normal distribution
+"""
+
+
 import numpy as np
 
 
-class MultiNormal():
-    """MultiNormal class"""
+class MultiNormal:
+    """
+    Represents a Multivariate Normal distribution
+    """
     def __init__(self, data):
-        """inits self and data"""
-        if not isinstance(data, np.ndarray):
+        """
+        Class constructor
+        """
+        if type(data) is not np.ndarray or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-        if len(data.shape) != 2:
-            raise TypeError("data must be a 2D numpy.ndarray")
-        if data.shape[1] < 2:
+
+        d, n = data.shape
+
+        if n < 2:
             raise ValueError("data must contain multiple data points")
 
-        def mean_cov(X):
-            """Mean and covariance.
-            Looked at how np does cov and found it to be fastest"""
-            if not isinstance(X, np.ndarray):
-                raise TypeError("X must be a 2D numpy.ndarray")
-            if len(X.shape) != 2:
-                raise TypeError("X must be a 2D numpy.ndarray")
-            if X.shape[1] < 2:
-                raise ValueError("X must contain multiple data points")
+        self.mean = np.mean(data, axis=1).reshape(d, 1)
 
-            X = X.T
-            mean = np.mean(X, axis=0, keepdims=1)
-            N = X.shape[0]
-            X -= mean[0]
-            X_T = X
-            c = np.matmul(X.T, X_T) / N
+        deviation = data - self.mean
 
-            # print(mean, "\n", c.squeeze())
-
-            return mean, c.squeeze()
-
-        self.mean, self.cov = mean_cov(data)
+        self.cov = np.matmul(deviation, deviation.T) / (n - 1)
 
     def pdf(self, x):
-        """finds the pdf of a multivariate normal distribution"""
-        if not isinstance(x, np.ndarray):
+        """
+        Calculates the PDF at a data point.
+        Returns the value of the PDF.
+        """
+        if type(x) is not np.ndarray:
             raise TypeError("x must be a numpy.ndarray")
-        d = x.shape[0]
-        if len(x.shape) != 2 or x.shape != (d, 1):
-            raise ValueError("x must have the shape ({}, 1)".format(d))
 
-        mean = self.mean[0]
+        d = self.cov.shape[0]
+
+        if len(x.shape) != 2:
+            raise ValueError('x must have the shape ({}, 1)'.format(d))
+
+        if x.shape[1] != 1 or x.shape[0] != d:
+            raise ValueError('x must have the shape ({}, 1)'.format(d))
+
+        n = x.shape[0]
+
+        mean = self.mean
         cov = self.cov
-        # print(mean, "\n", cov, "\n")
+        cov_det = np.linalg.det(cov)
+        cov_inv = np.linalg.inv(cov)
 
-        pdf = (1 / np.sqrt(((2 * np.pi) ** d) * np.linalg.det(cov)) *
-               np.exp(-1/2 * (x - mean).T * np.linalg.inv(cov) * (x - mean)))
-        pdf = np.sum(pdf) / d
-        return pdf
+        # denominator
+        den = np.sqrt(((2 * np.pi) ** n) * cov_det)
+
+        # exponential term
+        expo = -0.5 * np.matmul(np.matmul((x - mean).T, cov_inv), x - mean)
+
+        PDF = (1 / den) * np.exp(expo[0][0])
+
+        return PDF
