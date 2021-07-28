4148e3f7235f59d1b3a761a7406d30e4fe64a125
leocjj <leocj@hotmail.com>
2021-07-27 23:16:52 -0500

    Update multinormal.py

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -25,3 +25,30 @@ class MultiNormal():
         self.mean = data.mean(axis=1, keepdims=True)
         self.dev = data - self.mean
         self.cov = np.matmul(self.dev, self.dev.T) / (n - 1)
+
+    def pdf(self, x):
+        """
+        x is a numpy.ndarray of shape (d, 1) containing the data point whose
+            PDF should be calculated
+        d is the number of dimensions of the Multinomial instance
+        If x is not a numpy.ndarray, raise a TypeError with the message x must
+            be a numpy.ndarray
+        If x is not of shape (d, 1), raise a ValueError with the message x must
+            have the shape ({d}, 1)
+        Returns the value of the PDF
+        """
+        if not isinstance(x, np.ndarray):
+            raise TypeError("C must be a numpy.ndarray")
+        d = self.mean.shape[0]
+        if len(x.shape) != 2 or x.shape != (d, 1):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        res = np.exp(np.matmul(np.matmul((x - self.mean).T,
+                                         np.linalg.inv(self.cov)),
+                               (x - self.mean)
+                               ) / -2)\
+            / np.sqrt(pow(2 * np.pi, x.shape[0])
+                      * np.linalg.det(self.cov)
+                      )
+
+        return res[0][0]
