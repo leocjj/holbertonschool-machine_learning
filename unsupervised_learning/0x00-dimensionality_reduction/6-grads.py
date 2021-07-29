1358545a20cf814b2180b1c8098650f31d1356ad
leocjj <leocj@hotmail.com>
2021-07-28 23:52:25 -0500

    Create 6-grads.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Calculates the gradients of Y
+"""
+
+
+import numpy as np
+Q_affinities = __import__('5-Q_affinities').Q_affinities
+
+
+def grads(Y, P):
+    """
+    Returns: (dY, Q)
+    """
+    return None
