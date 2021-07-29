fe930cb36fd49291d363a6fff8e71689ddc72b73
leocjj <leocj@hotmail.com>
2021-07-28 23:53:54 -0500

    Update 2-P_init.py

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -9,7 +9,28 @@ import numpy as np
 
 
 def P_init(X, perplexity):
-    """
-    Returns: (D, P, betas, H)
-    """
-    return None
+    """Initializes the values D, P, betas, and H"""
+    n, d = X.shape
+
+    def dist(X):
+        """Finds the dist D"""
+        sum_X = np.sum(np.square(X), axis=1)
+        D = np.add(np.add(-2 * np.matmul(X, X.T), sum_X).T, sum_X)
+
+        np.fill_diagonal(D, 0)
+
+        return D
+
+    def entropy():
+        """Finds the shannon entropy H"""
+        return np.log2(perplexity)
+
+    D = dist(X)
+
+    P = np.zeros((n, n))
+
+    betas = np.ones((n, 1))
+
+    H = entropy()
+
+    return D, P, betas, H
