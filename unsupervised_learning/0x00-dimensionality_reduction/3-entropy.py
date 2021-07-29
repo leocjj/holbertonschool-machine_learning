f0c57630febdef5be0c2fcb3b4923492516af5a7
leocjj <leocj@hotmail.com>
2021-07-28 23:52:05 -0500

    Create 3-entropy.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/3-entropy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Calculates the Shannon entropy and P affinities relative to a data point
+"""
+
+
+import numpy as np
+
+
+def HP(Di, beta):
+    """
+    Returns: (Hi, Pi)
+    """
+    return None
