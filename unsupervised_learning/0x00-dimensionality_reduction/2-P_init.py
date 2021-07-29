b35469d8fc584630998a26dc1f230768ab00b603
leocjj <leocj@hotmail.com>
2021-07-28 23:49:11 -0500

    Create 2-P_init.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Initializes all variables required to calculate
+the P affinities in t-SNE:
+"""
+
+
+import numpy as np
+
+
+def P_init(X, perplexity):
+    """
+    Returns: (D, P, betas, H)
+    """
+    return None
