c6b793f9dfe8de9fbf08317e0678c1ef0fb1ea1f
leocjj <leocj@hotmail.com>
2021-07-28 23:52:32 -0500

    Create 7-cost.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/7-cost.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Calculates the cost of the t-SNE transformation
+"""
+
+
+import numpy as np
+
+
+def cost(P, Q):
+    """
+    Returns: C, the cost of the transformation
+    """
+    return None
