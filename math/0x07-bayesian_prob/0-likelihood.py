79ac8c04d85092585ffd439ee54cb1ec209ecb75
leocjj <leocj@hotmail.com>
2021-07-29 23:41:09 -0500

    Update 0-likelihood.py

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""The likelihood, bayesian"""
+""" 0x07. Bayesian Probability """
 import numpy as np
 
 
@@ -26,18 +26,28 @@ def likelihood(x, n, P):
     Returns: a 1D numpy.ndarray containing the likelihood of obtaining the
         data, x and n, for each probability in P, respec
     """
-    if not isinstance(n, int) or n < 1:
+    if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) and x < 0:
-        raise ValueError("x must be an integer that is",
-                         " greater than or equal to 0")
+
+    if type(x) is not int or x < 0:
+        err = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(err)
+
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+
+    if type(P) is not np.ndarray or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    if np.any(P > 1) or np.any(P < 0):
-        raise ValueError("All values in P must be in the range [0, 1]")
-    likelihoods = ((np.math.factorial(n)) / (np.math.factorial(x) *
-                   (np.math.factorial(n - x))) * P ** x * (1 - P) ** (n - x))
+
+    for prob in P:
+        if prob < 0 or prob > 1:
+            raise ValueError("All values in P must be in the range [0, 1]")
+
+    n_fact = np.math.factorial(n)
+    x_fact = np.math.factorial(x)
+    n_x_fact = np.math.factorial(n - x)
+
+    likelihoods = \
+        (n_fact / (x_fact * n_x_fact)) * (P ** x) * (1 - P) ** (n - x)
 
     return likelihoods
