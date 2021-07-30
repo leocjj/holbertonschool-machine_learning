0b2d15ad404b30b20c080799997b8583e0a49947
leocjj <leocj@hotmail.com>
2021-07-29 23:49:41 -0500

    Update 1-intersection.py

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""The likelihood, bayesian"""
+""" 0x07. Bayesian Probability """
 import numpy as np
 
 
 def intersection(x, n, P, Pr):
     """
-    calculates the intersection of obtaining this data with the various hypothetical probabilities:
+    calculates the intersection of obtaining this data with the various
+        hypothetical probabilities:
 
     x is the number of patients that develop severe side effects
     n is the total number of patients observed
