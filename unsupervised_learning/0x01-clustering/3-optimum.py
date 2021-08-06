325f5543e8e0e019ec9603368de9343a70745135
leocjj <leocj@hotmail.com>
2021-08-05 22:49:09 -0500

    Update 3-optimum.py

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -20,8 +20,8 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     Returns: results, d_vars, or None, None on failure
         results is a list containing the outputs of
             K-means for each cluster size
-        d_vars is a list containing the difference in variance from the smallest
-            cluster size for each cluster size
+        d_vars is a list containing the difference in variance from
+            the smallest cluster size for each cluster size
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2\
             or not isinstance(iterations, int) or iterations <= 0\
