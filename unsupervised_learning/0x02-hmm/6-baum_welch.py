4a7039a660a96c5220b868733b92df189270097a
leocjj <leocj@hotmail.com>
2021-08-12 23:54:44 -0500

    Create 6-baum_welch.py

---
+++ unsupervised_learning/0x02-hmm/6-baum_welch.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+""" 0x02. Hidden Markov Models """
+import numpy as np
+
+
+def forward(Observation, Emission, Transition, Initial):
+    """
+    performs the forward algorithm for a hidden markov model
+    """
+    if not isinstance(Observation, np.ndarray) or len(Observation.shape) != 1:
+        return None, None
+    if not isinstance(Emission, np.ndarray) or len(Emission.shape) != 2:
+        return None, None
+    if not isinstance(Transition, np.ndarray)\
+            or len(Transition.shape) != 2\
+            or Transition.shape[0] != Transition.shape[1]:
+        return None, None
+    if not isinstance(Initial, np.ndarray) or len(Initial.shape) != 2:
+        return None, None
+    if Emission.shape[0] != Transition.shape[0] != Transition.shape[0] !=\
+       Initial.shape[0]:
+        return None, None
+    if Initial.shape[1] != 1:
+        return None, None
+
+    T = Observation.shape[0]
+    N = Transition.shape[0]
+
+    alpha = np.zeros((N, T))
+    alpha[:, 0, np.newaxis] = (Initial.T * Emission[:, Observation[0]]).T
+
+    for t in range(1, Observation.shape[0]):
+        for j in range(Transition.shape[0]):
+            alpha[j, t] = (alpha[:, t - 1].dot(Transition[:, j]) *
+                           Emission[j, Observation[t]])
+
+    prob = np.sum(alpha[:, -1])
+
+    return prob, alpha
+
+
+def backward(Observation, Emission, Transition, Initial):
+    """
+    performs the backward algorithm for a hidden markov model
+    """
+    if not isinstance(Observation, np.ndarray) or len(Observation.shape) != 1:
+        return None, None
+    if not isinstance(Emission, np.ndarray) or len(Emission.shape) != 2:
+        return None, None
+    if not isinstance(Transition, np.ndarray)\
+            or len(Transition.shape) != 2\
+            or Transition.shape[0] != Transition.shape[1]:
+        return None, None
+    if not isinstance(Initial, np.ndarray) or len(Initial.shape) != 2:
+        return None, None
+    if Emission.shape[0] != Transition.shape[0] != Transition.shape[0] !=\
+       Initial.shape[0]:
+        return None, None
+    if Initial.shape[1] != 1:
+        return None, None
+
+    T = Observation.shape[0]
+    N = Transition.shape[0]
+
+    beta = np.zeros((N, T))
+    beta[:, T - 1] = np.ones((N))
+
+    for t in range(T - 2, -1, -1):
+        for j in range(N):
+            beta[j, t] = ((beta[:, t + 1]
+                           * Emission[:, Observation[t + 1]]).
+                          dot(Transition[j, :]))
+
+    likelihood = np.sum(np.sum(Initial.T
+                               * Emission[:, Observation[0]]
+                               * beta[:, 0]))
+
+    return likelihood, beta
+
+
+def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
+    """
+    performs the Baum-Welch algorithm for a hidden markov model
+    """
+    if not isinstance(Observations, np.ndarray) or len(Observations.shape) != 1:
+        return None, None
+    if not isinstance(Emission, np.ndarray) or len(Emission.shape) != 2:
+        return None, None
+    if not isinstance(Transition, np.ndarray)\
+            or len(Transition.shape) != 2\
+            or Transition.shape[0] != Transition.shape[1]:
+        return None, None
+    if not isinstance(Initial, np.ndarray) or len(Initial.shape) != 2:
+        return None, None
+    if Emission.shape[0] != Transition.shape[0] != Transition.shape[0] !=\
+       Initial.shape[0]:
+        return None, None
+    if Initial.shape[1] != 1:
+        return None, None
+
+    return Transition, Emission
