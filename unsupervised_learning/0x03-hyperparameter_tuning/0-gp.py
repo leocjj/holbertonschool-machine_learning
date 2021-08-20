989c238d1e9271aa1b7e038f98a143c58d5b99b5
leocjj <leocj@hotmail.com>
2021-08-19 23:09:25 -0500

    Update 0-gp.py

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env python3
 """
-http://krasserm.github.io/2018/03/19/gaussian-processes/
-Represents a noiseless 1D Gaussian process
+0x03. Hyperparameter Tuning
 """
 import numpy as np
 
 
 class GaussianProcess:
     """
-    Represents a noiseless 1D Gaussian process
+    class that represents a noiseless 1D Gaussian process
     """
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """
-        Class constructor
+        init of gp
         """
-        # (t, 1) inputs already sampled with the black-box function
         self.X = X_init
-        # (t, 1) outputs of the black-box function for each input in X
         self.Y = Y_init
-        # length parameter for the kernel
         self.l = l
-        # standard deviation given to the output of the black-box function
         self.sigma_f = sigma_f
-        # Current covariance kernel matrix for the Gaussian process
-        self.K = self.kernel(X_init, X_init)
+        self.K = self.kernel(self.X, self.X)
 
     def kernel(self, X1, X2):
         """
