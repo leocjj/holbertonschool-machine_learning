7e56ba735dcf524a1ce086836dc737736c158561
leocjj <leocj@hotmail.com>
2021-08-19 23:24:39 -0500

    Create 4-bayes_opt.py

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+0x03. Hyperparameter Tuning
+"""
+import numpy as np
+from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization():
+    """Bayesian optimization on a noiseless 1D Gaussian process"""
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
+                 sigma_f=1, xsi=0.01, minimize=True):
+        """init of BO"""
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.X_s = (np.linspace(start=bounds[0], stop=bounds[1],
+                                num=ac_samples)[:, np.newaxis])
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """ calculates the next best sample location """
+        mu, sigma = self.gp.predict(self.X_s)
+
+        if self.minimize is True:
+            mu_sample_opt = np.min(self.gp.Y)
+        else:
+            mu_sample_opt = np.max(self.gp.Y)
+
+        with np.errstate(divide="warn"):
+            imp = mu_sample_opt - mu - self.xsi
+            Z = imp / sigma
+            ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+            ei[ei == 0.0] = 0.0
+        X = self.X_s[np.argmax(ei, axis=0)]
+        return X, ei
