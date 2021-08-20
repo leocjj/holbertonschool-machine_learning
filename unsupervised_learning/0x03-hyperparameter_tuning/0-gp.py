d08af025a5adbdb8eb4372adf20a8c0acb93b32e
leocjj <leocj@hotmail.com>
2021-08-19 22:52:24 -0500

    Update 0-gp.py

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -4,9 +4,13 @@ import numpy as np
 
 
 class GaussianProcess():
-    """class that represents a noiseless 1D Gaussian process"""
+    """
+    class that represents a noiseless 1D Gaussian process
+    """
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
-        """init of gp"""
+        """
+        init of gp
+        """
         self.X = X_init
         self.Y = Y_init
         self.l = l
@@ -14,7 +18,9 @@ class GaussianProcess():
         self.K = self.kernel(self.X, self.X)
 
     def kernel(self, X1, X2):
-        """ calculates the covariance kernel matrix between two matrices """
+        """
+        calculates the covariance kernel matrix between two matrices
+        """
         cov = (np.sum(X1**2, 1).reshape(-1, 1)
                + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T))
         return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * cov)
