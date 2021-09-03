8df2966c3496670612c314cfdb64fa2f54dfd8bd
leocjj <leocj@hotmail.com>
2021-09-02 23:26:10 -0500

    Update 0-rnn_cell.py

--- supervised_learning/0x0D-RNNs/0-rnn_cell.py
+++ supervised_learning/0x0D-RNNs/0-rnn_cell.py
@@ -13,13 +13,9 @@ class RNNCell:
         """
         Class constructor
         """
-        # weight for the concatenated hidden state and input data
         self.Wh = np.random.randn(i + h, h)
-        # weight for the output
         self.Wy = np.random.randn(h, o)
-        # bias for the concatenated hidden state and input data
         self.bh = np.zeros((1, h))
-        # bias for the output
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
@@ -28,12 +24,8 @@ class RNNCell:
         Returns: h_next, y
         """
         concat = np.concatenate((h_prev, x_t), axis=1)
-
         h_next = np.tanh(concat @ self.Wh + self.bh)
-
-        # argument for softmax activation function
         soft = h_next @ self.Wy + self.by
-        # softmax activation function
         y = np.exp(soft) / np.sum(np.exp(soft), axis=1, keepdims=True)
 
         return h_next, y
