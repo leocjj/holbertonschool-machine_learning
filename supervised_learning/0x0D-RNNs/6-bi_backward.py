c4e5c08fddba42031d38b132f59975bf4510d694
leocjj <leocj@hotmail.com>
2021-09-02 23:39:45 -0500

    Task 6

---
+++ supervised_learning/0x0D-RNNs/6-bi_backward.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Represents a bidirectional cell of an RNN
+"""
+import numpy as np
+
+
+class BidirectionalCell:
+    """
+    Represents a bidirectional cell of an RNN
+    """
+    def __init__(self, i, h, o):
+        """
+        Class constructor
+        """
+        self.Whf = np.random.normal(size=(i + h, h))
+        self.Whb = np.random.normal(size=(i + h, h))
+        self.Wy = np.random.normal(size=(h + h, o))
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+        Calculates the hidden state in the forward direction for one time step.
+        Returns: h_next
+        """
+        concat = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(concat @ self.Whf + self.bhf)
+
+        return h_next
+
+    def backward(self, h_next, x_t):
+        """
+        Calculates the hidden state in the backward direction
+        for one time step.
+        Returns: h_prev
+        """
+        concat = np.concatenate((h_next, x_t), axis=1)
+        h_prev = np.tanh(concat @ self.Whb + self.bhb)
+
+        return h_prev
