90ec9e055bd76e9e13b065f2fbc55e51d0451caf
leocjj <leocj@hotmail.com>
2021-09-02 23:26:17 -0500

    Task 1

---
+++ supervised_learning/0x0D-RNNs/1-rnn.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Performs forward propagation for a simple RNN
+"""
+import numpy as np
+
+
+def rnn(rnn_cell, X, h_0):
+    """
+    Returns: H, Y
+    """
+    t, _, _ = X.shape
+
+    H = []
+    Y = []
+    H.append(h_0)
+
+    for step in range(t):
+        h_next, y = rnn_cell.forward(H[-1], X[step])
+        H.append(h_next)
+        Y.append(y)
+
+    H = np.array(H)
+    Y = np.array(Y)
+
+    return H, Y
