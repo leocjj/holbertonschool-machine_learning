6595891c0f2ab3799cc5b3a17a76044e046b7c7f
leocjj <leocj@hotmail.com>
2021-09-16 21:04:08 -0500

    Task 2

---
+++ supervised_learning/0x10-nlp_metrics/2-cumulative_bleu.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+0x10. Natural Language Processing - Evaluation Metrics
+"""
+import numpy as np
+
+
+def counter(phrase, n):
+    """Return dict with count of words"""
+    tuple_list = [(tuple(i for i in phrase[x:x + n]))
+                  for x in range(len(phrase) - n + 1)]
+    d = {}
+    for x in range(len(phrase) - n + 1):
+        key = tuple(i for i in phrase[x:x + n])
+        if key not in d:
+            d[key] = tuple_list.count(key)
+
+    return d
+
+
+def count_clip(references, sentence, n):
+    """Count clip"""
+    res = {}
+    ct_sentence = counter(sentence, n)
+    for ref in references:
+        ct_ref = counter(ref, n)
+        for k in ct_ref:
+            if k in res:
+                res[k] = max(ct_ref[k], res[k])
+            else:
+                res[k] = ct_ref[k]
+    count = {k: min(ct_sentence.get(k, 0), res.get(k, 0)) for k in ct_sentence}
+
+    return count
+
+
+def modified_precision(references, sentence, n):
+    """Modified precision"""
+    ct_clip = count_clip(references, sentence, n)
+    ct = counter(sentence, n)
+
+    return sum(ct_clip.values()) / float(max(sum(ct.values()), 1))
+
+
+def cumulative_bleu(references, sentence, n):
+    """
+    Calculates the cumulative n-gram BLEU score for a sentence:
+
+    references is a list of reference translations
+    each reference translation is a list of the words in the translation
+    sentence is a list containing the model proposed sentence
+    n is the size of the largest n-gram to use for evaluation
+    All n-gram scores should be weighted evenly
+    Returns: the cumulative n-gram BLEU score
+    """
+
+    value = 1 / n
+    W = [value for x in range(n)]
+    Pn = [modified_precision(references, sentence, ngram)
+          for ngram, _ in enumerate(W, start=1)]
+    c = len(sentence)
+    closest_ref_idx = np.argmin([abs(len(x) - c) for x in references])
+    r = len(references[closest_ref_idx])
+    if c > r:
+        BP = 1
+    else:
+        BP = np.exp(1 - (float(r) / c))
+    score = np.sum([(wn * np.log(Pn[i])) if Pn[i] != 0 else 0
+                    for i, wn in enumerate(W)])
+
+    return BP * np.exp(score)
