e865db4d124206acbeb453f54a254f31f42a746a
leocjj <leocj@hotmail.com>
2021-09-29 22:25:21 -0500

    Task 9

---
+++ supervised_learning/0x11-attention/9-transformer_encoder.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+0x11. Attention
+"""
+import tensorflow as tf
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
+
+
+class Encoder(tf.keras.layers.Layer):
+    """ Class Encoder """
+
+    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len,
+                 drop_rate=0.1):
+        """ Contructor method """
+        super().__init__()
+
+        self.dm = dm
+        self.N = N
+
+        self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
+
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
+                       for _ in range(N)]
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, training, mask):
+        """ call method """
+        seq_len = tf.shape(x)[1]
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        x += self.positional_encoding[:seq_len, :]
+        x = self.dropout(x, training=training)
+        for i in range(self.N):
+            x = self.blocks[i](x, training, mask)
+
+        return x
