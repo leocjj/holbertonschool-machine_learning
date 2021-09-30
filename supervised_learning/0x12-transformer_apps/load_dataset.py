5f7fde440b54ffc1aa7883a566c06c06f5b86f24
leocjj <leocj@hotmail.com>
2021-09-29 23:58:45 -0500

    Update load_dataset.py

--- supervised_learning/0x12-transformer_apps/load_dataset.py
+++ supervised_learning/0x12-transformer_apps/load_dataset.py
@@ -4,5 +4,5 @@ import tensorflow_datasets as tfds
 
 pt2en_train = tfds.load('ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
 for pt, en in pt2en_train.take(1):
-  print(pt.numpy().decode('utf-8'))
-  print(en.numpy().decode('utf-8'))
+    print(pt.numpy().decode('utf-8'))
+    print(en.numpy().decode('utf-8'))
