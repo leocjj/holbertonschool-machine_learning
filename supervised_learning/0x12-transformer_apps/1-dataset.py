e9ab7bad6c859b2cf630265f1bf1f76fe1b5c62d
leocjj <leocj@hotmail.com>
2021-09-30 00:27:14 -0500

    Update 1-dataset.py

--- supervised_learning/0x12-transformer_apps/1-dataset.py
+++ supervised_learning/0x12-transformer_apps/1-dataset.py
@@ -6,18 +6,19 @@ import tensorflow.compat.v2 as tf
 import tensorflow_datasets as tfds
 
 
-class Dataset():
-    '''class dataset'''
+class Dataset:
+    """ Class that loads and preps a dataset for machine translation """
+
     def __init__(self):
-        '''constructor
-        instance attributes:
-        data_train: contains the ted_hrlr_translate/pt_to_en tf.data.Dataset
-                    train split, loaded as_supervided
-        data_valid: contains the ted_hrlr_translate/pt_to_en tf.data.Dataset
-                    validate split, loaded as_supervided
-        tokenizer_pt: is the Portuguese tokenizer created from the training set
-        tokenizer_en: is the English tokenizer created from the training set
-        '''
+        """
+        Creates the instance attributes:
+        data_train, which contains the ted_hrlr_translate/pt_to_en
+            tf.data.Dataset train split, loaded as_supervided
+        data_valid, which contains the ted_hrlr_translate/pt_to_en
+            tf.data.Dataset validate split, loaded as_supervided
+        tokenizer_pt is the Portuguese tokenizer created from the training set
+        tokenizer_en is the English tokenizer created from the training set
+        """
         self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
                                     split='train', as_supervised=True)
         self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
@@ -27,32 +28,37 @@ class Dataset():
         self.tokenizer_en = tokenizer_en
 
     def tokenize_dataset(self, data):
-        '''creates sub-word tokenizers for our dataset
-        Args:
-        data: tf.data.Dataset whose examples are formatted as a tuple (pt, en)
-            pt is the tf.Tensor containing the Portuguese sentence
-            en is the tf.Tensor containing the corresponding English sentence
+        """
+        Creates sub-word tokenizers for our dataset:
+        data:
+            tf.data.Dataset whose examples are formatted as a tuple (pt, en)
+            pt: tf.Tensor containing the Portuguese sentence
+            en: tf.Tensor containing the corresponding English sentence
+        The maximum vocab size should be set to 2**15
         Returns: tokenizer_pt, tokenizer_en
         tokenizer_pt is the Portuguese tokenizer
         tokenizer_en is the English tokenizer
-        '''
-        token_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+        """
+        tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
                     (en.numpy() for pt, en in data), target_vocab_size=2**15)
 
-        token_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+        tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
                     (pt.numpy() for pt, en in data), target_vocab_size=2**15)
 
-        return token_pt, token_en
+        return tokenizer_en, tokenizer_pt
 
     def encode(self, pt, en):
-        '''encodes a translation into tokens
-        Args:
+        """
+        Encodes a translation into tokens:
         pt is the tf.Tensor containing the Portuguese sentence
         en is the tf.Tensor containing the corresponding English sentence
+        Tokenized sentences should include the start and end of sentence tokens
+        The start token should be indexed as vocab_size
+        The end token should be indexed as vocab_size + 1
         Returns: pt_tokens, en_tokens
-        pt_tokens is a np.ndarray containing the Portuguese tokens
-        en_tokens is a np.ndarray. containing the English tokens
-        '''
+            pt_tokens is a np.ndarray containing the Portuguese tokens
+            en_tokens is a np.ndarray. containing the English tokens
+        """
         pt_tokens = [self.tokenizer_pt.vocab_size] + self.tokenizer_pt.encode(
                     pt.numpy()) + [self.tokenizer_pt.vocab_size+1]
 
