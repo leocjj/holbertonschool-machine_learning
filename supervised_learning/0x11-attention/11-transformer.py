33da99d43eb5b5d432836484d14a0754939971d7
leocjj <leocj@hotmail.com>
2021-09-29 22:38:06 -0500

    Update 11-transformer.py

--- supervised_learning/0x11-attention/11-transformer.py
+++ supervised_learning/0x11-attention/11-transformer.py
@@ -8,11 +8,15 @@ Decoder = __import__('10-transformer_decoder').Decoder
 
 
 class Transformer(tf.keras.Model):
-    """ Class Transformer"""
+    """
+    Class Transformer
+    """
 
     def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
                  max_seq_input, max_seq_target, drop_rate=0.1):
-        """ Constructor method """
+        """
+        Constructor method
+        """
         super(Transformer, self).__init__()
         self.encoder = Encoder(N, dm, h, hidden,
                                input_vocab, max_seq_input, drop_rate)
@@ -22,7 +26,9 @@ class Transformer(tf.keras.Model):
 
     def call(self, inputs, target, training, encoder_mask, look_ahead_mask,
              decoder_mask):
-        """ call method """
+        """
+        call method
+        """
         enc_output = self.encoder(inputs, training, encoder_mask)
         dec_output, attention_weights = self.decoder(
             target, enc_output, training, look_ahead_mask, decoder_mask)
