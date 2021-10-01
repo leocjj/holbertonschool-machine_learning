08ecd876af631d2eb679dc1e75f75b0454f6f36d
leocjj <leocj@hotmail.com>
2021-09-30 19:36:27 -0500

    Task 5

---
+++ supervised_learning/0x12-transformer_apps/5-train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+0x12 Transformer applications
+"""
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+Dataset = __import__('3-dataset').Dataset
+create_masks = __import__('4-create_masks').create_masks
+Transformer = __import__('5-transformer').Transformer
+
+
+def train_transformer(N, dm, h, hidden, max_len, batch_size, epochs):
+    """ """
+    pass
