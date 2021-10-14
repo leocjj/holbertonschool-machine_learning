48e832248008cb3744ba3b23d09fec38ba139ca2
leocjj <leocj@hotmail.com>
2021-10-14 02:06:05 -0500

    Create 0-main.py

---
+++ supervised_learning/0x13-qa_bot/0-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+question_answer = __import__('0-qa').question_answer
+
+with open('ZendeskArticles/PeerLearningDays.md') as f:
+    reference = f.read()
+
+answer = question_answer('When are PLDs?', reference)
+print(answer)
