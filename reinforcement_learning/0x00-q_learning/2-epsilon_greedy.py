c1ab8088b8b7ec9f2493075fc107e9c690a19e79
leocjj <leocj@hotmail.com>
2021-10-26 23:48:37 -0500

    Update 2-epsilon_greedy.py

--- reinforcement_learning/0x00-q_learning/2-epsilon_greedy.py
+++ reinforcement_learning/0x00-q_learning/2-epsilon_greedy.py
@@ -7,9 +7,22 @@ load_frozen_lake = __import__('0-load_env').load_frozen_lake
 
 
 def epsilon_greedy(Q, state, epsilon):
+    """
+    Uses epsilon-greedy to determine the next action:
+    Q is a numpy.ndarray containing the q-table
+    state is the current state
+    epsilon is the epsilon to use for the calculation
+    You should sample p with numpy.random.uniformn to determine if your
+        algorithm should explore or exploit
+    If exploring, you should pick the next action with numpy.random.randint
+        from all possible actions
+    Returns: the next action index
+    """
     env = load_frozen_lake()
+
     if np.random.uniform(0, 1) < epsilon:
         action = env.action_space.sample()
     else:
         action = np.argmax(Q[state, :])
+
     return action
