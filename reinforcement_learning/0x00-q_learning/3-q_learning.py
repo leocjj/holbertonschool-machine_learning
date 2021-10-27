236ede974aec6dad0f02f9044b3cf16ccebe3c5a
leocjj <leocj@hotmail.com>
2021-10-26 23:52:19 -0500

    Create 3-q_learning.py

---
+++ reinforcement_learning/0x00-q_learning/3-q_learning.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+0x00. Q-learning
+"""
+import numpy as np
+epsilon_greedy = __import__("2-epsilon_greedy").epsilon_greedy
+
+
+def train(env,
+          Q,
+          episodes=5000,
+          max_steps=100,
+          alpha=0.1,
+          gamma=0.99,
+          epsilon=1,
+          min_epsilon=0.1,
+          epsilon_decay=0.05):
+    """
+    performs Q-learning:
+    env: is the FrozenLakeEnv instance
+    Q: is a numpy.ndarray containing the Q-table
+    episodes: is the total number of episodes to train over
+    max_steps: is the maximum number of steps per episode
+    alpha: is the learning rate
+    gamma: is the discount rate
+    epsilon: is the initial threshold for epsilon greedy
+    min_epsilon: is the minimum value that epsilon should decay to
+    epsilon_decay: is the decay rate for updating epsilon between episodes
+    When the agent falls in a hole, the reward should be updated to be -1
+    Returns: Q, total_rewards
+        Q: is the updated Q-table
+        total_rewards: is a list containing the rewards per episode
+    """
+
+    rewards = []
+    for _ in range(episodes):
+        state = env.reset()
+        if epsilon < min_epsilon:
+            epsilon = min_epsilon
+        for _ in range(max_steps):
+            action = epsilon_greedy(Q, state, epsilon)
+            new_state, reward, done, info = env.step(action)
+            Q[state][action] += epsilon_decay * (reward + gamma *
+                                                 np.max(Q[new_state]) -
+                                                 Q[state][action])
+            state = new_state
+            if done:
+                break
+        rewards.append(reward)
+
+    return Q, rewards
