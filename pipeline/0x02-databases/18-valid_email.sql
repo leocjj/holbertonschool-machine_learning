f85bc39390af84603b53187dc3fc4317f9153f42
leocjj <leocj@hotmail.com>
2021-12-06 21:26:56 -0500

    Create 18-valid_email.sql

---
+++ pipeline/0x02-databases/18-valid_email.sql
@@ -0,0 +1,10 @@
+-- Resets the attribute valid_email.
+DELIMITER //
+CREATE TRIGGER reset_email BEFORE UPDATE ON users
+	FOR EACH ROW
+	BEGIN
+		IF STRCMP(OLD.email, NEW.email) <> 0 THEN
+			SET NEW.valid_email = 0;
+        END IF;
+	END//
+DELIMITER ;
