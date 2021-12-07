791d10f0f1e934a9cb8717ecc97a18b3f86cf6c4
leocjj <leocj@hotmail.com>
2021-12-06 19:36:12 -0500

    Create 12-rating_genres.sql

---
+++ pipeline/0x02-databases/12-rating_genres.sql
@@ -0,0 +1,7 @@
+--  Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) rating
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings on tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.name
+ORDER BY 2 DESC;
