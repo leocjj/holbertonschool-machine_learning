da1d8d908795d5290920ab5f620e9d2f9c237305
leocjj <leocj@hotmail.com>
2021-12-06 22:14:57 -0500

    Update 32-update_topics.py

--- pipeline/0x02-databases/32-update_topics.py
+++ pipeline/0x02-databases/32-update_topics.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-'''changes all topics of a school documen'''
+"""changes all topics of a school documen"""
 import pymongo
 
 
 def update_topics(mongo_collection, name, topics):
-    '''def update_topics(mongo_collection, name, topics)
-    args:
-        mongo_collection will be the pymongo collection object
-        name (string) will be the school name to update
-        topics (list of strings) will be the list of topics approached
-            in the school
-    '''
+    """
+    mongo_collection will be the pymongo collection object
+    name (string) will be the school name to update
+    topics (list of strings) will be the list of topics approached
+        in the school
+    """
     mongo_collection.update({'name': name},
                             {'$set': {'topics': topics}},
                             multi=True)
