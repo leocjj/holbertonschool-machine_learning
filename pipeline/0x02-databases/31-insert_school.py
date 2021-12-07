43427bfa6bce1829e289bee93129c5f8343108b6
leocjj <leocj@hotmail.com>
2021-12-06 22:14:53 -0500

    Update 31-insert_school.py

--- pipeline/0x02-databases/31-insert_school.py
+++ pipeline/0x02-databases/31-insert_school.py
@@ -4,6 +4,7 @@ import pymongo
 
 
 def insert_school(mongo_collection, **kwargs):
-    """inserts a new document in a collection
+    """
+    Inserts a new document in a collection
     """
     return (mongo_collection.insert_one(kwargs).inserted_id)
